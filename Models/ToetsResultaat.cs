05db9fec71b4739d1378ec0c1e5ebafd9b79822b
sneakwish <wishal_wishnu@hotmail.com>
2021-11-25 00:45:12 +0100

    6

---
+++ Models/ToetsResultaat.cs
@@ -0,0 +1,11 @@
+public class ToetsResultaat{
+    string StudentNaam;
+    string Cijfer;
+
+    public ToetsResultaat(string StudentNaam, string Cijfer){
+        this.StudentNaam = StudentNaam;
+        this.Cijfer = Cijfer;
+
+    }
+}
+
